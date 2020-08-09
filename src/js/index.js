c3d12f3f7ba941662b0f23f87e393b1b89db5a91
Przemysław Grochowina <pgrochowina@gmail.com>
2020-08-09 09:55:05 +0200

    feat(js): add functions for toggle light/dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -19,16 +19,37 @@ if (entry) {
 
 save.addEventListener("click", () => {
   localStorage.setItem("myMemo", notepad.value);
-  //e.preventDefault(); //zapobiega odświeżaniu formularza, u mnie się to nie dzieje - nie wiem, dlaczego
+  e.preventDefault(); //zapobiega odświeżaniu formularza
 });
 
 load.addEventListener("click", () => {
   notepad.value = localStorage.getItem("myMemo");
-  //e.preventDefault(); //zapobiega odświeżaniu formularza, u mnie się to nie dzieje - nie wiem, dlaczego
+  //e.preventDefault(); //zapobiega odświeżaniu formularza
 });
 
 copy.addEventListener("click", () => {
   notepad.select();
   document.execCommand('copy');
-  //e.preventDefault(); //zapobiega odświeżaniu formularza, u mnie się to nie dzieje - nie wiem, dlaczego
+  //e.preventDefault(); //zapobiega odświeżaniu formularza
+});
+
+//tryb jasny-ciemny
+
+let isDark = false;
+
+const viewMode = document.querySelector(".button__toggle--js");
+const mainHeader = document.querySelector(".header--js");
+
+viewMode.addEventListener("click", () => {
+  if (isDark == false) {
+    document.documentElement.style.setProperty("--primary-color", "#282c35");
+    document.documentElement.style.setProperty("--secondary-color", "#ffa7c4");
+    isDark = true;
+    console.log(`zmieniono tryb na ciemny, wartość isDark = ${isDark}`);
+  } else if (isDark == true) {
+    document.documentElement.style.setProperty("--primary-color", "white");
+    document.documentElement.style.setProperty("--secondary-color", "#d23669");
+    isDark = false;
+    console.log(`zmieniono tryb na jasny, wartość isDark = ${isDark}`);
+  }
 });
